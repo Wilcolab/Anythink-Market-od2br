3f1fcc806a523efdb9fd841f950252f4ac0f7e03
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-07-21 20:05:42 +0100

    fix broken image

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import agent from "../agent";
-import { connect } from "react-redux";
-import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import agent from '../agent'
+import { connect } from 'react-redux'
+import { ITEM_FAVORITED, ITEM_UNFAVORITED } from '../constants/actionTypes'
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -15,30 +15,30 @@ const mapDispatchToProps = (dispatch) => ({
       type: ITEM_UNFAVORITED,
       payload: agent.Items.unfavorite(slug),
     }),
-});
+})
 
 const ItemPreview = (props) => {
-  const item = props.item;
+  const item = props.item
 
   const handleClick = (ev) => {
-    ev.preventDefault();
+    ev.preventDefault()
     if (item.favorited) {
-      props.unfavorite(item.slug);
+      props.unfavorite(item.slug)
     } else {
-      props.favorite(item.slug);
+      props.favorite(item.slug)
     }
-  };
+  }
 
   return (
     <div
       className="card bg-dark border-light p-3"
-      style={{ borderRadius: "20px" }}
+      style={{ borderRadius: '20px' }}
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image || '/placeholder.png'}
         className="card-img-top item-img"
-        style={{ borderRadius: "20px" }}
+        style={{ borderRadius: '20px' }}
       />
       <div className="card-body">
         <Link to={`/item/${item.slug}`} className="text-white">
@@ -59,7 +59,7 @@ const ItemPreview = (props) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default connect(() => ({}), mapDispatchToProps)(ItemPreview);
+export default connect(() => ({}), mapDispatchToProps)(ItemPreview)
