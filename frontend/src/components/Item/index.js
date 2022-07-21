54c031aac2b4f7cdc252ece35df21a94d5443fee
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-07-21 21:01:38 +0100

    prettify

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -1,23 +1,23 @@
-import ItemMeta from './ItemMeta'
-import CommentContainer from './CommentContainer'
-import React from 'react'
-import agent from '../../agent'
-import { connect } from 'react-redux'
-import marked from 'marked'
+import ItemMeta from "./ItemMeta";
+import CommentContainer from "./CommentContainer";
+import React from "react";
+import agent from "../../agent";
+import { connect } from "react-redux";
+import marked from "marked";
 import {
   ITEM_PAGE_LOADED,
   ITEM_PAGE_UNLOADED,
-} from '../../constants/actionTypes'
+} from "../../constants/actionTypes";
 
 const mapStateToProps = (state) => ({
   ...state.item,
   currentUser: state.common.currentUser,
-})
+});
 
 const mapDispatchToProps = (dispatch) => ({
   onLoad: (payload) => dispatch({ type: ITEM_PAGE_LOADED, payload }),
   onUnload: () => dispatch({ type: ITEM_PAGE_UNLOADED }),
-})
+});
 
 class Item extends React.Component {
   componentWillMount() {
@@ -26,37 +26,37 @@ class Item extends React.Component {
         agent.Items.get(this.props.match.params.id),
         agent.Comments.forItem(this.props.match.params.id),
       ])
-    )
+    );
   }
 
   componentWillUnmount() {
-    this.props.onUnload()
+    this.props.onUnload();
   }
 
   render() {
     if (!this.props.item) {
-      return null
+      return null;
     }
 
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
-    }
+    };
     const canModify =
       this.props.currentUser &&
-      this.props.currentUser.username === this.props.item.seller.username
+      this.props.currentUser.username === this.props.item.seller.username;
     return (
       <div className="container page">
         <div className="text-dark">
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image || '/placeholder.png'}
+                src={this.props.item.image || "/placeholder.png"}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{
-                  height: '500px',
-                  width: '100%',
-                  borderRadius: '6px',
+                  height: "500px",
+                  width: "100%",
+                  borderRadius: "6px",
                 }}
               />
             </div>
@@ -70,7 +70,7 @@ class Item extends React.Component {
                   <span className="badge badge-secondary p-2 mx-1" key={tag}>
                     {tag}
                   </span>
-                )
+                );
               })}
             </div>
           </div>
@@ -85,8 +85,8 @@ class Item extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Item)
+export default connect(mapStateToProps, mapDispatchToProps)(Item);
