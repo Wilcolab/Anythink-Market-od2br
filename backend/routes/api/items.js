f2a07ae1f24694504792c9a36c78484201f78a49
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-10-18 14:58:45 +0100

    search query

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,6 +53,10 @@ router.get('/', auth.optional, function (req, res, next) {
     query.tagList = { $in: [req.query.tag] }
   }
 
+  if (typeof req.query.title !== 'undefined') {
+    query.title = new RegExp(req.query.title, 'i')
+  }
+
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
     req.query.favorited
@@ -100,7 +104,6 @@ router.get('/', auth.optional, function (req, res, next) {
 })
 
 router.get('/feed', auth.required, function (req, res, next) {
-  var query = {}
   var limit = 20
   var offset = 0
 
@@ -110,11 +113,6 @@ router.get('/feed', auth.required, function (req, res, next) {
 
   if (typeof req.query.offset !== 'undefined') {
     offset = req.query.offset
-  }
-
-  if (typeof req.query.title !== 'undefined') {
-    query.title = req.query.title
-    query.title = new RegExp(req.query.title, 'i')
   }
 
   User.findById(req.payload.id).then(function (user) {
