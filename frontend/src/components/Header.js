a4fc1c9e733f16c2a31560680005e4c9d1282e01
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-10-26 12:35:17 +0100

    fix formatting

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../imgs/topbar_logo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../imgs/topbar_logo.png";
 
 const LoggedOutView = () => {
   return (
@@ -37,7 +37,11 @@ const LoggedInView = (props) => {
 
       <li className="nav-item">
         <Link to={`/@${props.currentUser.username}`} className="nav-link">
-          <img src={props.currentUser.image} className="user-pic pr-1" alt={props.currentUser.username} />
+          <img
+            src={props.currentUser.image}
+            className="user-pic pr-1"
+            alt={props.currentUser.username}
+          />
           {props.currentUser.username}
         </Link>
       </li>
@@ -48,11 +52,18 @@ const LoggedInView = (props) => {
 const Header = (props) => {
   const { currentUser } = props;
   return (
-    <nav className="navbar navbar-expand-md navbar-dark" style={{ padding: '0.5rem 2rem' }}>
+    <nav
+      className="navbar navbar-expand-md navbar-dark"
+      style={{ padding: "0.5rem 2rem" }}
+    >
       <Link to="/" className="navbar-brand">
         <img alt="logo" src={logo} />
       </Link>
-      {currentUser ? <LoggedInView currentUser={currentUser} /> : <LoggedOutView currentUser={currentUser} />}
+      {currentUser ? (
+        <LoggedInView currentUser={currentUser} />
+      ) : (
+        <LoggedOutView currentUser={currentUser} />
+      )}
     </nav>
   );
 };
