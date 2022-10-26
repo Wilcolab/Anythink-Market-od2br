f8a2bbd9d004ab4d7e6ab6e44520282d5af761b5
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-10-26 12:22:47 +0100

    refactor header component from class component to functional component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../imgs/topbar_logo.png";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../imgs/topbar_logo.png'
 
 const LoggedOutView = () => {
   return (
@@ -17,8 +17,8 @@ const LoggedOutView = () => {
         </Link>
       </li>
     </ul>
-  );
-};
+  )
+}
 
 const LoggedInView = (props) => {
   return (
@@ -46,28 +46,26 @@ const LoggedInView = (props) => {
         </Link>
       </li>
     </ul>
-  );
-};
-
-class Header extends React.Component {
-  render() {
-    return (
-      <nav
-        className="navbar navbar-expand-md navbar-dark"
-        style={{ padding: "0.5rem 2rem" }}
-      >
-        <Link to="/" className="navbar-brand">
-          <img alt="logo" src={logo} />
-        </Link>
-
-        {this.props.currentUser ? (
-          <LoggedInView currentUser={this.props.currentUser} />
-        ) : (
-          <LoggedOutView currentUser={this.props.currentUser} />
-        )}
-      </nav>
-    );
-  }
+  )
 }
 
-export default Header;
+const Header = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-md navbar-dark"
+      style={{ padding: '0.5rem 2rem' }}
+    >
+      <Link to="/" className="navbar-brand">
+        <img alt="logo" src={logo} />
+      </Link>
+
+      {this.props.currentUser ? (
+        <LoggedInView currentUser={this.props.currentUser} />
+      ) : (
+        <LoggedOutView currentUser={this.props.currentUser} />
+      )}
+    </nav>
+  )
+}
+
+export default Header
