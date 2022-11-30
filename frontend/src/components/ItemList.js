d99cd459e10025c3fe45e4d854ba788a6d97ca77
Shedrack akintayo <akintayoshedrack@gmail.com>
2022-11-30 20:21:14 +0100

    fix

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -21,26 +21,24 @@ const ItemList = (props) => {
     }
     return <div className="py-4 no-items"> No items are here... yet.</div>;
   }
-};
+  return (
+    <div className="container py-2">
+      <div className="row">
+        {props.items.map((item) => {
+          return (
+            <div className="col-sm-4 pb-2" key={item.slug}>
+              <ItemPreview item={item} />
+            </div>
+          );
+        })}
+      </div>
 
-return (
-  <div className="container py-2">
-    <div className="row">
-      {props.items.map((item) => {
-        return (
-          <div className="col-sm-4 pb-2" key={item.slug}>
-            <ItemPreview item={item} />
-          </div>
-        );
-      })}
+      <ListPagination
+        pager={props.pager}
+        itemsCount={props.itemsCount}
+        currentPage={props.currentPage}
+      />
     </div>
-
-    <ListPagination
-      pager={props.pager}
-      itemsCount={props.itemsCount}
-      currentPage={props.currentPage}
-    />
-  </div>
-);
-
+  );
+};
 export default ItemList;
