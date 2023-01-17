f867448fb14f2448b8e809c7939719e5c9048513
Shedrack akintayo <akintayoshedrack@gmail.com>
2023-01-17 11:28:47 +0100

    refactored banner component to use styledcomponents

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
 import logo from "../../imgs/logo.png";
+
+const StyledBanner = styled.div`
+  color: white;
+  background-color: ${(props) => props.theme.dark};
+`;
+
+const theme = {
+  dark: "#000000",
+};
 
 const Banner = () => {
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span>A place to </span>
-          <span id="get-part">get</span>
-          <span> the cool stuff.</span>
+    <ThemeProvider theme={theme}>
+      <StyledBanner>
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+            <span>A place to </span>
+            <span id="get-part">get</span>
+            <span> the cool stuff.</span>
+          </div>
         </div>
-      </div>
-    </div>
+      </StyledBanner>
+    </ThemeProvider>
   );
 };
 
