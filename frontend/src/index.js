da2c239261c09dd0442d6d017f257170fa0ec0ee
vanessa-cooper-1 <ness+1@anythink.family>
2023-01-24 10:23:29 +0000

    Dataseeding

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,21 +6,15 @@ import { store, history } from "./store";
 
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
-import { ThemeProvider } from "styled-components";
 
 import App from "./components/App";
-import GlobalStyle from "./styles/GlobalStyle";
 
-const theme = {};
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
     </ConnectedRouter>
   </Provider>,
 
